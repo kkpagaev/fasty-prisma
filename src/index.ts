2d832abdc714c24b16629a7af453d728f2d4b658
kkpagaev <kkpagaev@gmail.com>
2023-06-27 23:51:10 +0300

    feat(prisma): added prisma and inited basicis

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,56 @@
 import fastify from "fastify"
 
-const r = fastify()
+import { PrismaClient } from "@prisma/client"
 
-r.get("/", async () => {
-  return {
-    hello: "world!",
-  }
-})
+const prisma = new PrismaClient()
 
-// Run the server!
-const start = async () => {
-  try {
-    await r.listen({ port: 3000 })
-  } catch (err) {
-    r.log.error(err)
-    process.exit(1)
+async function createUser() {
+  const user = await prisma.user.create({
+    data: {
+      name: "Vlad",
+      email: "vlad@prisma.io",
+    },
+  })
+  console.log(user)
+  return user
+}
+async function main() {
+  const r = fastify()
+
+  r.get("/users", async () => {
+    const users = await prisma.user.findMany()
+    return users
+  })
+
+  r.post("/users", async () => {
+    return await createUser()
+  })
+
+  r.get("/", async () => {
+    return {
+      hello: "world!",
+    }
+  })
+
+  // Run the server!
+  const start = async () => {
+    try {
+      await r.listen({ port: 3000 })
+    } catch (err) {
+      r.log.error(err)
+      process.exit(1)
+    }
   }
+
+  await start()
 }
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-start()
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
